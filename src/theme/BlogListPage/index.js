2cf03fd25fa5ef1b1da6a09c81a612a04381fefd
Jaroslav Šmolík <grissius@gmail.com>
2023-01-15 19:52:09 +0100

    Change tags layout

--- src/theme/BlogListPage/index.js
+++ src/theme/BlogListPage/index.js
@@ -29,15 +29,15 @@ function BlogListPageContent(props) {
   const {metadata, items, sidebar} = props;
   return (
     <BlogLayout sidebar={sidebar}>
-      <div>
       <p>Vítejte na rozcestníku článků. Pro snazší orientaci jsou rozděleny do tematických skupin, které Vám stručně představím:  </p>
-      <p><strong><a href="/blog/tags/ne-plodnost">(NE)PLODNOST</a></strong>: je základní informační sekce věnovaná plodnosti a jejím poruchám. Najdete zde nejběžnější důvody, které k poruchám plodnosti vedou. Mojí snahou bude, aby články v této sekci tématicky a logicky navazovaly. Najdete zde i popis jednotlivých vyšetření, která mohou příčinu zhoršené plodnosti objasnit. </p>
-      <p><strong><a href="/blog/tags/planeta-pomoci">PALETA POMOCI</a></strong>: má za úkol přiblížit možnosti, ze kterých neplodné páry volí, pokud se rozhodnou k řešení přizvat někoho dalšího.  Své místo zde mají metody asistované reprodukce, metody podpůrné (masáže, aromaterapie, akupunktura,...), psychoterapie a další. Věřím, že se s různými formami pomoci setkáme též ve Vašich příbězích. </p>
-      <p><strong><a href="/blog/tags/vyzvy-pro-dusi">VÝZVY PRO DUŠI</a></strong>: neplodnost pokládám za nevyžádanou možnost k seberozvoji. Chystám sem články o oblastech našeho života, které jsou neplodností ovlivněny a o práci se sebou, která nám může v konečném důsledku pomoci nejen v řešení neplodnosti.</p>
-      <p><strong><a href="/blog/tags/kontroverze">KONTROVERZE</a></strong>: Všimla jsem si myšlenkové brzdy, která se mi aktivuje ve spojistosti s některými aspekty řešení neplodnosti, často se jedná o témata vyvolávající celospolečenskou debatu. Mými kontroverzemi čekajícími na zpracování nyní jsou:  adopce jako kompenzace bezdětnosti, věk pacientek podstupujících léčbu neplodnosti, rodičovství plánované jednou osobou, dárcovství vajíček a spermií, využití náhradní matky. Předpokládám, že další témata na sebe nenechají dlouho čekat. </p>
+      <div class="posts-hub">
+      <div><strong><a href="/blog/tags/ne-plodnost"><img src="https://i.imgur.com/aqQ8MZB.png" />(NE)PLODNOST</a></strong></div>
+      <div><strong><a href="/blog/tags/planeta-pomoci"><img src="https://i.imgur.com/qrbbYxR.png" />PALETA POMOCI</a></strong></div>
+      <div><strong><a href="/blog/tags/vyzvy-pro-dusi"><img src="https://i.imgur.com/9aLgNik.png" />VÝZVY PRO DUŠI</a></strong></div>
+      <div><strong><a href="/blog/tags/kontroverze"><img src="https://i.imgur.com/Gnj9E0X.png" />KONTROVERZE</a></strong></div>
       </div>
-      <BlogPostItems items={items} />
-      <BlogListPaginator metadata={metadata} />
+      {/* <BlogPostItems items={items} />
+      <BlogListPaginator metadata={metadata} /> */}
     </BlogLayout>
   );
 }
